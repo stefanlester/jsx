cf427492299d684d9e75b59ecfbb0bb346db8648
stefanlester <38575516+stefanlester@users.noreply.github.com>
2020-11-18 02:44:54 +0100

    Complete

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const ApprovalCard = () => {
+const ApprovalCard = props => {
+
+    //log props from approval card
+    console.log(props.children);
+
   return (
     <div className="ui card">
-      <div className="content">Are you sure?</div>
+      <div className="content">{props.children}</div> {/* passing properties from Approval Card in index.js */}
       <div className="extra content">
         <div className="ui two buttons">
           <div className="ui basic green button">Approve</div>
